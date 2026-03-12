f7c1a944259eb70b76ead1de0c700798d5d6e07a
Crimor2026 <cam_01sag@hotmail.com>
2026-03-12 18:47:38 -0500

    Boton reportes abre Excel SharePoint

--- frontend/landing/landing.js
+++ frontend/landing/landing.js
@@ -88,7 +88,7 @@ function irReportes(){
     document.body.classList.add("pageFadeOut");
 
     setTimeout(()=>{
-        window.location.href="/reportes";
+        window.location.href="https://atugobpe-my.sharepoint.com/:x:/r/personal/hvillon_atu_gob_pe/_layouts/15/Doc.aspx?sourcedoc=%7BDAFC0B4C-6A12-400D-9D2F-7EFA1E9F02E4%7D&file=Registro%20de%20intervenciones.xlsx&action=default&mobileredirect=true";
     },350);
 
 }
--- frontend/reportes/reportes.js
+++ frontend/reportes/reportes.js
@@ -1,1 +1,8 @@
-console.log("Modulo de reportes cargado");
+function irReportes(){
+
+window.open(
+"https://atugobpe-my.sharepoint.com/:x:/r/personal/hvillon_atu_gob_pe/_layouts/15/Doc.aspx?sourcedoc=%7BDAFC0B4C-6A12-400D-9D2F-7EFA1E9F02E4%7D&file=Registro%20de%20intervenciones.xlsx&action=default&mobileredirect=true",
+"_blank"
+);
+
+}
